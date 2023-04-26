daab27da2a51183b025a7e6eba7c6574adf03144
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-04-26 12:20:34 +0200

    committing component navbar, created routes, links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
+import { Route, Routes } from "react-router-dom";
 import logo from "./assets/logo-projet-2.png";
 import Home from "./pages/Home";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -8,8 +10,13 @@ function App() {
       <header>
         <img src={logo} className="logo-site" alt="logo site" />
       </header>
-      <Home />
-      <p>coucou</p>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={Home} />
+        <Home />
+        <p>coucou</p>
+        <Navbar />
+      </Routes>
     </div>
   );
 }
