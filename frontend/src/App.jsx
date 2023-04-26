41dcc2b55bb612369de594b3a9bdbf155e17b5c4
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-04-26 16:15:43 +0200

    modified Home, changed files lock yaml

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
+import logo from "./assets/logo-projet-2.png";
 import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <header>
+        <img src={logo} className="logo-site" alt="logo site" />
+      </header>
       <Home />
       <p>coucou</p>
     </div>
