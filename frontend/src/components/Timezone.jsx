bedd60dada6b432913c2b3014b8e9faacd49e5d7
Girbau <lgirbau36@gmail.com>
2023-05-11 10:58:32 +0200

    api timezone fonctionel

--- frontend/src/components/Timezone.jsx
+++ frontend/src/components/Timezone.jsx
@@ -1,9 +1,55 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function Timezone() {
+  const [query, setQuery] = useState("");
+  const [timezoneName, setTimezoneName] = useState("");
+  const [timezone, setTimezone] = useState("");
+  useEffect(() => {
+    async function fetchTimezone() {
+      try {
+        const response = await axios.get(
+          `https://api.opencagedata.com/geocode/v1/json?q=${query}&key=${
+            import.meta.env.VITE_TIMEZONE_API_KEY
+          }`
+        );
+        const timezoneNameResponse =
+          response.data.results[0].annotations.timezone.name;
+        setTimezoneName(timezoneNameResponse);
+        const timezoneOffset =
+          response.data.results[0].annotations.timezone.offset_string;
+        const hours = timezoneOffset.slice(0, -2) - 2;
+        const minutes = timezoneOffset.slice(-2);
+        const formattedOffset = `${hours}H${minutes}`;
+        setTimezone(formattedOffset);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchTimezone();
+  }, [query]);
+  function HandleSearch(e) {
+    e.preventDefault();
+  }
   return (
-    <figure>
-      <img src="/time.jpg" alt="time" />
-      <p>Décalage Horaire</p>
-    </figure>
+    <div>
+      <form onSubmit={HandleSearch}>
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <figure>
+        <img src="/time.jpg" alt="time" />
+        <p>Region:</p>
+        <p>{timezoneName}</p>
+        <p>décalage horaire de:</p>
+        <p>{timezone}</p>
+      </figure>
+    </div>
   );
 }
 export default Timezone;
