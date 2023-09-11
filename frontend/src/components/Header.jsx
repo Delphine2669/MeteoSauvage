3afa03cfa2340b1b0c04fff4e1268560d330bb4c
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-09-11 14:35:33 +0200

    attempt to add meta

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 import Navbar from "./Navbar";
 import SearchBar from "./SearchBar";
+import logo from "../assets/logo.png";
 
 function Header({ setCitySearch }) {
   return (
     <header>
       <div className="header-div">
-        <img src="/logo-projet-2.png" className="logo-site" alt="logo site" />
+        <img src={logo} className="logo-site" alt="logo site" />
       </div>
       <Navbar />
       <SearchBar setCitySearch={setCitySearch} />
