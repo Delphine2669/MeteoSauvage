baea34ef3c7d315b0b8dbdc4ba0d0c9afc93bb72
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-09-11 15:52:00 +0200

    trying to add image to open graph

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <div className="footer">
-      <p>&copy; Copyright 2023 Météo sauvage tous droits resérvés.</p>
+      <p>&copy; Copyright 2023 Météo sauvage tous droits réservés.</p>
     </div>
   );
 }
