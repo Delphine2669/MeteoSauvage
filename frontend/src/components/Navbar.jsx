a4e3b861baad5328d801458ddab75850c5b21ed1
@Delphine2669 <delphine.thevenet2669@gmail.com>
2024-02-06 21:30:51 +0100

    changed media queries

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,30 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <div>
       <nav className="design-nav-block">
-        <Link to="/" className="design-nav home-button">
-          Home
-        </Link>
-        <Link to="/meteo" className="design-nav weather-button">
-          Météo
-        </Link>
-        <Link to="/decalage-horaire" className="design-nav timezone-button">
-          Décalage Horaire
-        </Link>
-        <Link to="/devises" className="design-nav currency-button">
-          Devises
-        </Link>
+        <button type="button" className="design-nav home-button">
+          <NavLink to="/" className="design-nav-home">
+            Home
+          </NavLink>
+        </button>
+
+        <button type="button" className="design-nav weather-button">
+          <NavLink to="/meteo" className="design-nav-weather">
+            Météo
+          </NavLink>
+        </button>
+        <button type="button" className="design-nav timezone-button">
+          <NavLink to="/decalage-horaire" className="design-nav-timezone">
+            Décalage Horaire
+          </NavLink>
+        </button>
+        <button type="button" className="design-nav currency-button">
+          <NavLink to="/devises" className="design-nav-currency">
+            Devises
+          </NavLink>
+        </button>
       </nav>
     </div>
   );
